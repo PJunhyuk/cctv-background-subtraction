27ebd1cf59166f6d765deb70178b3f018d993e18
PJunhyuk <jgravity@naver.com>
2018-01-25 13:38:41 +0900

    Generate video_bg_image

--- demo.py
+++ demo.py
@@ -11,6 +11,13 @@ from moviepy.editor import *
 ## default
 import sys
 
+## counter
+import collections
+
+import numpy as np
+
+from PIL import Image
+
 ## ArgParse
 parser = ap.ArgumentParser()
 parser.add_argument('-f', '--videoFileName', help='Name of Video File')
@@ -23,19 +30,33 @@ else:
     print('You have to input videoFileName')
     sys.exit(1)
 
+## Read video
 video_file_route = 'testset/' + video_file_name
 print('video_file_route: ' + video_file_route)
 video = VideoFileClip(video_file_route)
 print('Read Complete')
 
-frame_index = 0
+video = video.resize(width = 32)
 
-while(1):
-    ret, frame = video.read()
-    if ret == False:
-        break
+video_frame_number = int(video.duration * video.fps) ## duration: second / fps: frame per second
+video_bg_row = []
+video_bg = []
 
-    print('frame_index: ' + str(frame_index))
-    frame_index = frame_index + 1
-
-    break
+for a in range(0, video.size[1]):
+    for b in range(0, video.size[0]):
+        pixel_list = []
+        print(a)
+        print(b)
+        for i in range(0, video_frame_number):
+            # Save i-th frame as image
+            image = video.get_frame(i/video.fps)
+            pixel = (image[a][b][0], image[a][b][1], image[a][b][2])
+            pixel_list.append(pixel)
+        pixel_list_counter = collections.Counter(pixel_list)
+        pixel_list_mode = pixel_list_counter.most_common(1)[0][0]
+        video_bg_row.append(pixel_list_mode)
+    video_bg.append(video_bg_row)
+    print(str(a+1) + "_Time(s): " + str(time.time() - time_start))
+video_bg_image = Image.fromarray(video_bg)
+video_bg_image.save('video_bg.jpg')
+print("Time(s): " + str(time.time() - time_start))
